42e7694072639805bfa9574eec14696e3f881ce8
Joebert Cerezo <joebertcerezo@gmail.com>
2026-08-21 10:31:49 +0800

    feat: add input.rs

--- src/input.rs
+++ src/input.rs
@@ -1,4 +1,6 @@
-use crossterm::event::KeyEvent;
+use std::{io::Result, time::Duration};
+
+use crossterm::event::{KeyCode, KeyEvent, KeyEventKind};
 
 use crate::models::direction::Direction;
 
@@ -11,14 +13,35 @@ pub enum AppCommand {
 }
 
 pub fn parse_key_event(key: KeyEvent) -> Option<AppCommand> {
+    if key.kind != KeyEventKind::Press {
+        return None;
+    }
+
     match key.code {
-        crossterm::event::KeyCode::Up => Some(AppCommand::Move(Direction::Up)),
-        crossterm::event::KeyCode::Down => Some(AppCommand::Move(Direction::Down)),
-        crossterm::event::KeyCode::Left => Some(AppCommand::Move(Direction::Left)),
-        crossterm::event::KeyCode::Right => Some(AppCommand::Move(Direction::Right)),
-        crossterm::event::KeyCode::Char('p') => Some(AppCommand::Pause),
-        crossterm::event::KeyCode::Char('r') => Some(AppCommand::Restart),
-        crossterm::event::KeyCode::Char('q') => Some(AppCommand::Quit),
+        KeyCode::Esc => Some(AppCommand::Quit),
+        KeyCode::Up | KeyCode::Char('w') | KeyCode::Char('W') => {
+            Some(AppCommand::Move(Direction::Up))
+        }
+        KeyCode::Down | KeyCode::Char('s') | KeyCode::Char('S') => {
+            Some(AppCommand::Move(Direction::Down))
+        }
+        KeyCode::Left | KeyCode::Char('a') | KeyCode::Char('A') => {
+            Some(AppCommand::Move(Direction::Left))
+        }
+        KeyCode::Right | KeyCode::Char('d') | KeyCode::Char('D') => {
+            Some(AppCommand::Move(Direction::Right))
+        }
+        KeyCode::Char('p') | KeyCode::Char('P') => Some(AppCommand::Pause),
+        KeyCode::Char('r') | KeyCode::Char('R') => Some(AppCommand::Restart),
         _ => None,
     }
+}
+
+pub fn poll_command(timeout: Duration) -> Result<Option<AppCommand>> {
+    if crossterm::event::poll(timeout)? {
+        if let crossterm::event::Event::Key(key_event) = crossterm::event::read()? {
+            return Ok(parse_key_event(key_event));
+        }
+    }
+    Ok(None)
 }
